3942109d73cbb2ac0eb601ca6012c5d39066e8da
KMSkelton <kmskelton@outlook.com>
2019-02-22 22:36:17 -0800

    apparently pushing to heroku isn't sufficient

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hgvf.wsgi --log-file -
--- hgvf/settings.py
+++ hgvf/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hgvf.urls'
@@ -138,5 +139,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
